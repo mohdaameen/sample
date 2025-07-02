12240c9c82a83ac272f3d14d94aeccde3714625c
AHMAD Mohd Aameen Ext OBS/CTIO <jfvd7538@orange.com>
2025-07-03 00:12:48 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Hello from GitHub Pages 👋</h1>
+      <p>This is a Vite + React app deployed on GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
